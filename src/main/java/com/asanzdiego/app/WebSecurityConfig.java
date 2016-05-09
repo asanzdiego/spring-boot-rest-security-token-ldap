78240f2c51acacd6b76731a32034b1ca2ec0fb48
Adolfo Sanz De Diego <asanzdiego@gmail.com>
2016-05-09 13:48:12 +0200

    cambios en el pom.xml

--- src/main/java/com/asanzdiego/app/WebSecurityConfig.java
+++ src/main/java/com/asanzdiego/app/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.configurers.ldap.LdapAuthenticationProviderConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 
@@ -15,6 +16,7 @@ import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.web.cors.CorsUtils;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
